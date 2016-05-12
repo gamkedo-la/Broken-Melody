d4986c6ee494ac82035c135bfb4c507c2b1b9695
Jeremy Kenyon <lanthos@gmail.com>
2016-05-12 16:49:55 -0700

    Centered the new awesome startScreen image

--- js/game/main.js
+++ js/game/main.js
@@ -68,7 +68,7 @@ window.onload = function() {
           canvasContext.fillText("Bonuse Keys:" ,400, 340);
           canvasContext.fillText(numberOfKeys ,480, 340);
         } else {
-          canvasContext.drawImage(startScreen, 0, 0);
+          canvasContext.drawImage(startScreen, 0, 70);
         }
       }
     }, 1000/framesPerSecond);
