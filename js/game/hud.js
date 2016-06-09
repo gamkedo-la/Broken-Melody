0f5770cf34e650038f3a64fc51f74789ca06dd45
Chris Karpyszyn <chris.karpyszyn@gmail.com>
2016-06-09 15:02:57 -0400

    Hud changes. More white boxes. Better spacing.

--- js/game/hud.js
+++ js/game/hud.js
@@ -1,19 +1,54 @@
 
 function drawTransparentHudBox() {
     hudCanContext.globalAlpha = 0.75;
+    //pizza box :)
+    colorRectHud(0, 0, 180, 67, "white");
+    //cash box
     colorRectHud(377, 0, 50, 46, "white");
-    colorRectHud(MUTE_BUTTON_X, MUTE_BUTTON_Y, MUTE_BUTTON_WIDTH-2, MUTE_BUTTON_HEIGHT-2, "white");
+    //weapon box
+    colorRectHud(canvas.width-130, 0, 140, 64, "white");
+    //mute box
+    colorRectHud(MUTE_BUTTON_X, MUTE_BUTTON_Y, MUTE_BUTTON_WIDTH - 2, MUTE_BUTTON_HEIGHT - 2, "white");
     hudCanContext.globalAlpha = 1;
+}
+
+var offset = 15;
+function drawAmmo() {
+    var AMMO_TEXT_SIZE = 15;
+    ///change to holdingPistol
+    if (hasPistol) {
+        drawHudText(pistolAmmo, canvas.width - 95 + offset, 46, `bold ${AMMO_TEXT_SIZE}px Consolas MS`, "#000000", "center");
+    }
+    if (hasRifle) {
+        drawHudText(rifleAmmo, canvas.width - 50 + offset, 46, `bold ${AMMO_TEXT_SIZE}px Consolas MS`, "#000000", "center");
+    }
+}
+
+function drawWeapons() {
+    drawAmmo();
+    if (hasRifle) {
+        hudCanContext.drawImage(tileRifleHudPic, canvas.width - 80 + offset, 0);
+    }
+    if (hasPistol) {
+        hudCanContext.drawImage(tilePistolHudPic, canvas.width - 140 + offset, 0);
+    }
+
+    if (holdingPistol === true) {
+        rectHud(canvas.width - 135 + offset, 2, 52, 50, 2, "#EF5353");
+    } else if (holdingRifle === true) {
+        rectHud(canvas.width - 80 + offset, 2, 52, 50, 2, "#EF5353");
+    }
+
 }
 
 function displayRemainingDeliveries() {
     if (isWinner) {
         return;
     }
-    var textY = 80;
-    hudCanContext.fillStyle = "yellow";
-    hudCanContext.font = "20px Consolas MS";
-    hudCanContext.fillText("Remaining Deliveries: " + remainingDeliveries, 110, textY);
+    var textY = 65;
+    hudCanContext.fillStyle = "black";
+    hudCanContext.font = "bold 14px Consolas MS";
+    hudCanContext.fillText(`DELIVERIES LEFT: ${remainingDeliveries}`, 85, textY);
 }
 
 
@@ -74,33 +109,9 @@ function drawFundsAndTimer() {
 
 
 //WEAPONS
-function drawWeapons() {
-    drawAmmo();
-    if (hasRifle) {
-        hudCanContext.drawImage(tileRifleHudPic, canvas.width - 80, 0);
-    }
-    if (hasPistol) {
-        hudCanContext.drawImage(tilePistolHudPic, canvas.width - 135, 0);
-    }
 
-    if (holdingPistol === true) {
-        rectHud(canvas.width - 130, 2, 52, 50, 2, "yellow");
-    } else if (holdingRifle === true) {
-        rectHud(canvas.width - 80, 2, 52, 50, 2, "yellow");
-    }
 
-}
 
-function drawAmmo() {
-    var AMMO_TEXT_SIZE = 15;
-    ///change to holdingPistol
-    if (hasPistol) {
-        drawHudText(pistolAmmo, canvas.width - 90, 15, `${AMMO_TEXT_SIZE}px Consolas MS`, "#000000", "center");
-    }
-    if (hasRifle) {
-        drawHudText(rifleAmmo, canvas.width - 50, 15, `${AMMO_TEXT_SIZE}px Consolas MS`, "#000000", "center");
-    }
-}
 
 
 function drawSpeaker() {
